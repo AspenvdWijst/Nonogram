efaf999154f14f170d3b522560e2a82573cfed8f
Ryan <s1216264@student.windesheim.nl>
2025-03-29 12:41:47 +0100

    LoginForm klaar + json file en Commentaar compleet.

---
+++ Nonogram/LoginForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Nonogram
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            btnLogin = new Button();
+            btnRegister = new Button();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(336, 174);
+            label1.Name = "label1";
+            label1.Size = new Size(106, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(336, 223);
+            label2.Name = "label2";
+            label2.Size = new Size(99, 30);
+            label2.TabIndex = 1;
+            label2.Text = "Password";
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(509, 282);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(131, 40);
+            btnLogin.TabIndex = 2;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click_1;
+            // 
+            // btnRegister
+            // 
+            btnRegister.Location = new Point(336, 282);
+            btnRegister.Name = "btnRegister";
+            btnRegister.Size = new Size(131, 40);
+            btnRegister.TabIndex = 3;
+            btnRegister.Text = "Register";
+            btnRegister.UseVisualStyleBackColor = true;
+            btnRegister.Click += btnRegister_Click_1;
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(465, 169);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(175, 35);
+            txtUsername.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(465, 220);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(175, 35);
+            txtPassword.TabIndex = 5;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1167, 606);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(btnRegister);
+            Controls.Add(btnLogin);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            Load += LoginForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button btnLogin;
+        private Button btnRegister;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+    }
+}
